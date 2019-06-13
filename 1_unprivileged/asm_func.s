ad562ddbb73dc93193a780f9b807a4173b9a5956
sks3a09 <sks3a09@gmail.com>
2019-06-13 16:30:16 +0800

    finish

--- 1_unprivileged/asm_func.s
+++ 1_unprivileged/asm_func.s
@@ -2,24 +2,36 @@
 
 .global	read_sp
 read_sp:
-	??????
+	mov r0,sp
+	bx lr
 
 .global	read_msp
 read_msp:
-	??????
+	mrs r0,msp
+	bx lr
 
 .global	read_psp
 read_psp:
-	??????
+	mrs r0,psp
+	bx lr
 
 .global	read_ctrl
 read_ctrl:
-	??????
+	mrs r0,control
+	bx lr
 
 .global	start_user
 start_user:
-	??????
+	movs lr,r0
+	msr psp,r1
+	movs r2,#0b11
+	msr control,r2
+	isb
+	bx lr
 
 .global	sw_priv
 sw_priv:
-	??????
+	movs r2,#0
+	msr control,r2
+	isb
+	bx lr
